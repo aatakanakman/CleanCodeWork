1ee8b5ef9887be78f187f8a4a7b2a3ebf87e07f2
aatakanakman <48566734+aatakanakman@users.noreply.github.com>
2021-05-25 15:33:57 +0300

    Work 8

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
 
 
+//Tamplate Engine
+app.set("view engine", "ejs");
+
 
 //Middleware
 app.use(express.static('public'))
 
+
+//ROUTES
 app.get('/', (req, res) => {
 
-    res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+    res.render('index')
+})
+app.get('/about', (req, res) => {
 
+    res.render('about')
+})
+app.get('/add_post', (req, res) => {
 
+    res.render('add_post')
 })
 
 const port = 3000;
