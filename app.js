eaf28748ae884f5c8218ad3605b91f0e1cf073be
aatakanakman <48566734+aatakanakman@users.noreply.github.com>
2021-05-25 12:46:16 +0300

    Static files uploaded

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+
+//Middleware
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+
+    res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+
+
+})
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Servis ${port} 'unda dinleniyor.`)
+})
